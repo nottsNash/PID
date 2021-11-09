6081130fdfae6bd850bd39e6c3367bd5884240c8
nottsNash <73956488+nottsNash@users.noreply.github.com>
2021-11-09 14:33:23 +0000

    added in control of the motor from the proportional controller

--- PID.ino
+++ PID.ino
@@ -10,7 +10,13 @@
 # define LINE_SENSOR_UPDATE 50
 # define MOTOR_UPDATE       200
 # define ENCODER_UPDATE     200
+# define PID_UPDATE         20
 
+//motors
+# define L_PWM_PIN 10
+# define L_DIR_PIN 16
+# define R_PWM_PIN 9
+# define R_DIR_PIN 15
 
 // declaration on class instances
 Motors_c  leftMotor;
@@ -18,12 +24,20 @@ Motors_c  rightMotor;
 Motors_c MotorsGeneral;
 LineSensor_c  LineSensors;
 Calibrate_c CalibrationClass;
+PID_c  pidClass;
 
-
+//change
 //------------Global variables--------------//
 unsigned long current_ts = 0;
 unsigned long encoder_ts = 0;
+unsigned long motor_ts = 0;
+unsigned long PID_ts = 0;
 
+int leftPWM = 0; //PWM values used to control the power going to the motors
+int rightPWM = 0; //values sent to motors must be int's but calculations done with floats
+int leftPWMfloat = 0;
+int rightPWMfloat = 0;
+int maxPWM = 100;
 float LeftVelocity = 0;
 float RightVelocity = 0;
 
@@ -42,6 +56,8 @@ float Yg = 0;
 float localAngle = 0;
 float GlobalAngle = 0;
 
+float FeedbackSignal = 0;
+float TargetVelocity = 1;
 
 // ----------------------------put your setup code here, to run once:
 void setup() {
@@ -53,15 +69,30 @@ void setup() {
   setupEncoder0();
   setupEncoder1();
   
+  leftMotor.initialise(L_PWM_PIN,L_DIR_PIN); //inititalise motors with correct pin layout
+  rightMotor.initialise(R_PWM_PIN,R_DIR_PIN);
+  
+  pidClass.initialisePIDs();
+  
   }
 
 //-------------------------------------------------------------loop-----------------------------------------------------------------------------------  
 void loop() {
  
-  unsigned long elapsed_t;
+  unsigned long elapsed_ts;
   current_ts = millis();
-  elapsed_t = current_ts - encoder_ts;
-  if( elapsed_t > ENCODER_UPDATE ){
+  
+ 
+  elapsed_ts = current_ts - PID_ts;
+  if( elapsed_ts > PID_UPDATE ){
+    FeedbackSignal = updatePID(TargetVelocity, LeftVelocity);
+  }
+
+
+
+
+  elapsed_ts = current_ts - encoder_ts;
+  if( elapsed_ts > ENCODER_UPDATE ){
   RotationsRight = (count_encoder_right/358.3)*-1;
   RotationsLeft = (count_encoder_left/358.3)*-1;
   DistanceRight = RotationsRight * WheelCircumference; //in mm
@@ -84,5 +115,20 @@ void loop() {
 
   encoder_ts = millis();
   }
+  
+  elapsed_ts = current_ts - motor_ts;
+  if( elapsed_ts > MOTOR_UPDATE ) {
+     //proportional controller used for line following
+     leftPWM = FeedbackSignal;
+     rightPWM = 0;
+     leftPWM = leftMotor.rescaler(leftPWM, maxPWM); //scales values greater than PWM value 50 to 50 for the left motor only 
+     rightPWM = rightMotor.rescaler(rightPWM, maxPWM);
+ 
+     rightMotor.Direction(rightPWM, R_DIR_PIN, R_PWM_PIN);
+     leftMotor.Direction(leftPWM, L_DIR_PIN, L_PWM_PIN); //reverses the motor direction for negative values of PWM
+     motor_ts = millis();
+  }
+
+  
   
 }
--- pid.h
+++ pid.h
@@ -1,24 +1,24 @@
 // this #ifndef stops this file
 // from being included mored than
-// once by the compiler. 
+// once by the compiler.
 #ifndef _PID_H
 #define _PID_H
 
 // Class to contain generic PID algorithm.
 class PID_c {
   public:
-  
+
     // Constructor, must exist.
     PID_c() {
 
-    } 
+    }
 
     float Kp;
     float FeedbackSignal;
     float errorSignal;
 
     void initialisePIDs(){
-      Kp = 2;
+      Kp = 10;
       FeedbackSignal = 0;
     }
 
